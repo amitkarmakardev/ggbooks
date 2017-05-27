deea018bf210984cb5b2221f3be302e158d3a73c
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-05-27 09:07:54 +0000

    Create fallback primary ip

--- searchISBNwgenerator_latest.php
+++ searchISBNwgenerator_latest.php
@@ -1,7 +1,7 @@
 <?php
 
 set_time_limit(0);
-ini_set('memory_limit', '2048M');
+ini_set('memory_limit', '1024M');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -15,6 +15,17 @@ require "helpers/generateBookDetails.php";
 $start = $argv[1];
 $limit = $argv[2];
 $outbound_ip = $argv[3];
+
+$fallback_primary_ip = '192.168.100.100';
+
+if($start == null || $start == '' || $limit == null || $limit == ''){
+    echo "Please provide correct ISBN Numbers";
+    die();
+}
+
+if($outbound_ip == null || $outbound_ip == ''){
+    $outbound_ip = $fallback_primary_ip;
+}
 
 for ($x = $start; $x <= $limit; $x++) {
     //pads string to 9 chars long
