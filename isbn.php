c2f81ca8fdad3fa6832d9b415773177453927556
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-05 10:47:38 +0000

    Configure project to use libs/isbn api

--- isbn.php
+++ isbn.php
@@ -1,6 +1,9 @@
 <?php
+
 global $config;
+
 require "settings/bootstrap.php";
+
 $benchmarks = [];
 processArguments($argv);
 switch (strtoupper($config['option'])) {
@@ -17,12 +20,14 @@ switch (strtoupper($config['option'])) {
 function generateAll()
 {
     global $config;
-
     validateISBNParts($config['start'], $config['limit']);
-
+    $isbn = new Isbn\Isbn();
+    
     for ($isbn_part = intval($config['start']); $isbn_part <= intval($config['limit']); $isbn_part++) {
-        $isbn10 = generateISBN10($isbn_part);
+        $interimISBN = str_pad($isbn_part, 9, '0', STR_PAD_LEFT);
+        $isbn10 = $interimISBN.$isbn->checkDigit->make10($interimISBN);
+        $isbn13 = $isbn->translate->to13($isbn10);
         generateBookDetails($isbn10);
-        generateClassifyDetails($isbn10);
+        generateClassifyDetails($isbn13);
     }
 }
--- settings/bootstrap.php
+++ settings/bootstrap.php
@@ -4,14 +4,20 @@ ini_set('memory_limit', '1024M');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+
 require 'libs/kint-master/Kint.class.php';
 require 'libs/simple_html_dom.php';
+require 'libs/Isbn/Isbn.php';
+require 'libs/Isbn/Hyphens.php';
+require 'libs/Isbn/Check.php';
+require 'libs/Isbn/CheckDigit.php';
+require 'libs/Isbn/Translate.php';
+require 'libs/Isbn/Validation.php';
 
 require 'helpers/benchmark.php';
 require 'helpers/database_functions.php';
 require 'helpers/gbooks_scraper.php';
 require 'helpers/url_functions.php';
-require 'helpers/code_generator.php';
 require 'helpers/oclc_scraper.php';
 require 'helpers/common_functions.php';
 
