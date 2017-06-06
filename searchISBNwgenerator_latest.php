801d03158f32fca25624a59062b25c584e3c43a4
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-06 22:52:37 +0530

    Rename isbn

--- searchISBNwgenerator_latest.php
+++ searchISBNwgenerator_latest.php
@@ -1,67 +1,33 @@
 <?php
 
-set_time_limit(0);
-ini_set('memory_limit', '1024M');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+global $config;
 
-require 'libs/kint-master/Kint.class.php';
-require 'libs/simple_html_dom.php';
+require "settings/bootstrap.php";
 
-require 'helpers/benchmark.php';
-require 'helpers/database_functions.php';
-require 'helpers/gbooks_scraper.php';
-require 'helpers/url_functions.php';
-require 'helpers/code_generator.php';
-require 'helpers/oclc_scraper.php';
-
-
-// Define global variable
 $benchmarks = [];
-$config = require "settings/config.php";
-$outbound_ip = $config['default_ip'];
-
-if (count($argv) < 2) {
-    echo "Too few argument";
-    die();
+processArguments($argv);
+switch (strtoupper($config['option'])) {
+    case "-C":
+        generateClassifyDataFromDatabase();
+        break;
+    case "-G":
+        generateAll();
+        break;
+    default:
+        die("Invalid option: ".$config['option'].PHP_EOL);
 }
 
-if (count($argv) >= 2) {
-    $option = strtoupper(trim($argv[1]));
-
-    switch ($option) {
-        case "--CLASSIFY":
-            generateClassifyDataFromDatabase();
-            break;
-        case "--GENERATE":
-            generateAll($argv);
-            break;
-        default:
-            echo "Invalid option $option" . PHP_EOL;
-            break;
-    }
-}
-
-function generateAll($argv)
+function generateAll()
 {
-    global $outbound_ip;
-    if (count($argv) > 2) {
-
-        $start = $argv[2];
-        $limit = $argv[3];
-
-        if (intval($start) > intval($limit)) {
-            die("Start is greater than end!" . PHP_EOL);
-        }
-
-        if (count($argv) > 4) {
-            $outbound_ip = $argv[4];
-        }
-
-        for ($isbn_part = intval($start); $isbn_part <= intval($limit); $isbn_part++) {
-            $isbn10 = generateISBN10($isbn_part);
-            generateBookDetails($isbn10);
-            generateClassifyDetails($isbn10);
-        }
+    global $config;
+    validateISBNParts($config['start'], $config['limit']);
+    $isbn = new Isbn\Isbn();
+    
+    for ($isbn_part = intval($config['start']); $isbn_part <= intval($config['limit']); $isbn_part++) {
+        $interimISBN = str_pad($isbn_part, 9, '0', STR_PAD_LEFT);
+        $isbn10 = $interimISBN.$isbn->checkDigit->make10($interimISBN);
+        $isbn13 = $isbn->translate->to13($isbn10);
+        generateBookDetails($isbn10);
+        generateClassifyDetails($isbn13);
     }
 }
