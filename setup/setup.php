8edf05154374c03ab023e654528284343566df56
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-14 09:28:48 +0530

    Remove unnecessary code

--- setup/setup.php
+++ setup/setup.php
@@ -10,14 +10,6 @@ if (count($argv) > 1) {
     $config['db_credentials']['mysql_db'] = trim($argv[1]);
 }
 
-$sql = "SHOW DATABASES LIKE '{$config['db_credentials']['mysql_db']}'";
-
-$result = executeQuery("SHOW DATABASES LIKE '{$config['db_credentials']['mysql_db']}'")->fetchObject();
-
-if($result->Database == $config['db_credentials']['mysql_db']){
-    pLog("Database {$config['db_credentials']['mysql_db']} already exists");
-    die();
-}
 // Create database
 require __DIR__."/database/create_database.php";
 
