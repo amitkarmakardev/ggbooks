52b5772f8d56cec82400db7b071ff01054407bd8
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-17 10:05:11 +0530

    Add http response code to the tables

--- setup/migrations/1_create_book_details_table.php
+++ setup/migrations/1_create_book_details_table.php
@@ -1,19 +1,20 @@
 <?php
 
 $sql = "CREATE TABLE `book_details` (
-        `id` int NOT NULL AUTO_INCREMENT,
-        `title` text DEFAULT NULL,
-        `author` text DEFAULT NULL,
-        `edition` text DEFAULT NULL,
-        `publisher` text DEFAULT NULL,
-        `publish_year` text DEFAULT NULL,
-        `isbn10` varchar(10) NOT NULL UNIQUE,
-        `isbn13` varchar(13) DEFAULT NULL,
-        `page_length` varchar(20) DEFAULT NULL,
-        `subjects` text DEFAULT NULL,
-        `price` varchar(50) DEFAULT NULL,
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `isbn10` VARCHAR(10) NOT NULL,
+        `isbn13` VARCHAR(13) NOT NULL,
+        `title` TEXT DEFAULT NULL,
+        `author` TEXT DEFAULT NULL,
+        `edition` TEXT DEFAULT NULL,
+        `publisher` TEXT DEFAULT NULL,
+        `publish_year` TEXT DEFAULT NULL,
+        `page_length` VARCHAR(20) DEFAULT NULL,
+        `subjects` TEXT DEFAULT NULL,
+        `price` VARCHAR(50) DEFAULT NULL,
         `sample` BOOLEAN DEFAULT 0,
-        PRIMARY KEY(ID)
+        `http_response_code` VARCHAR(10) DEFAULT NULL,
+        PRIMARY KEY(id)
       ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci";
 
 return $sql;
--- setup/migrations/2_create_summary_classify_table.php
+++ setup/migrations/2_create_summary_classify_table.php
@@ -1,17 +1,19 @@
 <?php
 
 $sql = "CREATE TABLE summary_classify (
-          id int(20) AUTO_INCREMENT,
-          isbn10 varchar(50),
-          oclc varchar(50),
-          ddc text,
-          class_number_ddc text,
-          holdings_ddc text,
-          links_ddc text,
-          lcc text,
-          class_number_lcc text,
-          holdings_lcc text,
-          links_lcc text,
-          PRIMARY KEY  (id)
+          `id` INT(20) AUTO_INCREMENT,
+          `isbn10` VARCHAR(10) NOT NULL,
+          `isbn13` VARCHAR(13) NOT NULL,
+          `oclc` VARCHAR(20) DEFAULT NULL,
+          `ddc` TEXT DEFAULT NULL,
+          `class_number_ddc` TEXT DEFAULT NULL,
+          `holdings_ddc` TEXT DEFAULT NULL,
+          `links_ddc` TEXT DEFAULT NULL,
+          `lcc` TEXT DEFAULT NULL,
+          `class_number_lcc` TEXT DEFAULT NULL,
+          `holdings_lcc` TEXT DEFAULT NULL,
+          `links_lcc` TEXT DEFAULT NULL,
+          `http_response_code` VARCHAR(10) DEFAULT NULL,
+          PRIMARY KEY (id)
         ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci";
 return $sql;
